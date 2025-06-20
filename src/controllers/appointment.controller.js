54dd4f86c909d1589e314a868036c3d5b6edc686
Mohit Bhardwaj <mohitshukla0986@gmail.com>
2025-06-20 11:12:19 +0530

    fix some issue

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -17,6 +17,9 @@ const createAppointment = asyncHandler(async (req, res) => {
   const {id:doctorId} = req.params;
 
   // Check if doctor exists
+  console.log(doctorId);
+  console.log("challa");
+  
   const doctorExists = await DoctorProfile.findOne({_id:doctorId});
   if (!doctorExists) {
     throw new ApiError(404, "Doctor not found");
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -210,8 +210,8 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullName, email } = req.body;
 
-  if (!fullName || !email) {
-    throw new ApiError(400, "All fields are required");
+  if (!fullName && !email) {
+    throw new ApiError(400, "one field is required");
   }
 
   const user = await User.findByIdAndUpdate(
@@ -244,7 +244,7 @@ const updateAvatar = asyncHandler(async (req, res) => {
   }
 
   const avatarUploadResult = await uploadOnCloudinary(fileLocalPath);
-
+  
   if (!avatarUploadResult?.url) {
     throw new ApiError(500, "Error uploading image");
   }
